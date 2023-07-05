e60fb3c274ce52ec7f47bbe87898ed11140b4b09
Pranto Paul <108918430+imprantopaul@users.noreply.github.com>
2023-07-05 14:09:24 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,71 @@
+// ১. Variable কি ?
+
+variable is a name of storage location.
+
+// ২. Variable কিভাবে লিখতে হয় ?
+
+var x = "";
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+string type variable is string value; like: "jone due";
+
+Example: var str = "jone due";
+console.log(str);
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+number type variable is just number; like: 123456789;
+Example: var num = 123456789;
+console.log(num);
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+Boolean type variable is true and false; 
+Example: 
+var x = true;
+var y = false;
+console.log(x, y);
+
+
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+var toUpp = 'abcdefghijklmnopqrstuvwxyz';
+var result = "to UpperCase = " + toUpp.toUpperCase();
+console.log(result);
+var toLow = 'ABCDFGHIJKLMNOPQRSTUVWXYZ';
+var result2 = "to LowerCase = " + toLow.toLowerCase();
+console.log(result2);
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+javascript operator list is big but the most used operator is +, - , *, /, %, **, ++, --, ==, ===. ETC 
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন ।
+var x = -55;
+var y = Math.abs(x);
+console.log(y);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন ।
+
+var x = 2.30;
+var y = Math.ceil(x);
+console.log(y);
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+var x = 2.999990;
+var y = Math.floor(x);
+console.log(y);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+var x = 2.4;
+var y = Math.round(x);
+console.log(y);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+For Ludu Game:
+
+var x = Math.floor(Math.random() * 6) + 1;
+console.log("Random Game = " + x)
+
+// asignment.js - GitHub Link 
